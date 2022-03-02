64ddb9f9f6cdc53844460c2753c0023e30629196
Anuj Bansal <highlightanuj@gmail.com>
2022-03-02 14:59:24 +0530

    Add copilot manifests, update dockerfile

--- api/app.js
+++ api/app.js
@@ -1,35 +1,46 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var uuid = require('node-uuid');
+const { v4: uuidv4 } = require("uuid");
 
-var pg = require('pg');
-var conString = process.env.DB; // "postgres://username:password@localhost/database";
+var { Pool } = require("pg");
+
+let connectionString = "";
+let pool;
+if (process.env.DBCLUSTER_SECRET) {
+  const { username, host, dbname, password, port } = JSON.parse(
+    process.env.DBCLUSTER_SECRET
+  );
+  connectionString = `postgres://${username}:${password}@${host}:${port}/${dbname}`;
+  console.log(connectionString);
+  pool = new Pool({
+    user: username,
+    database: dbname,
+    password: password,
+    host: host,
+    port: port,
+  });
+} else {
+  connectionString = process.env.DB; // "postgres://username:password@localhost/database";
+  pool = new Pool({ connectionString });
+}
 
 // Routes
-app.get('/api/status', function(req, res) {
-  pg.connect(conString, function(err, client, done) {
-    if(err) {
-      return res.status(500).send('error fetching client from pool');
+app.get("/api/status", function (req, res) {
+  pool.query("SELECT now() as time", (error, results) => {
+    if (error) {
+      console.log("error connecting to db");
+      return res.status(500).send("error connecting to db");
     }
-    client.query('SELECT now() as time', [], function(err, result) {
-      //call `done()` to release the client back to the pool
-      done();
-
-      if(err) {
-        return res.status(500).send('error running query');
-      }
-
-      return res.json({
-        request_uuid: uuid.v4(),
-        time: result.rows[0].time
-      });
+    return res.json({
+      request_uuid: uuidv4(),
+      time: results.rows[0].time,
     });
   });
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+app.use(function (req, res, next) {
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -38,25 +49,24 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+if (app.get("env") === "development") {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.json({
       message: err.message,
-      error: err
+      error: err,
     });
   });
 }
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.json({
     message: err.message,
-    error: {}
+    error: {},
   });
 });
-
 
 module.exports = app;
--- web/routes/index.js
+++ web/routes/index.js
@@ -1,26 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var request = require('request');
+var request = require("request");
 
-
-var api_url = process.env.API_HOST + '/api/status';
+var api_url = process.env.API_HOST + "/api/status";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get("/", function (req, res, next) {
   request(
     {
-      method: 'GET',
+      method: "GET",
       url: api_url,
-      json: true
+      json: true,
     },
     function (error, response, body) {
       if (error || response.statusCode !== 200) {
-        return res.status(500).send('error running request to ' + api_url);
+        return res
+          .status(500)
+          .send("Error running request to API host: " + api_url);
       } else {
-        res.render('index', {
-          title: '3tier App' ,
+        res.render("index", {
+          title: "3tier App",
           request_uuid: body.request_uuid,
-          time: body.time
+          time: body.time,
         });
       }
     }
